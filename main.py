6cd14876265adeea23963ceac13ee8dc72bf3723
roroal23 <rodrigo.najar.portaro@gmail.com>
2025-10-24 10:58:47 +0200

    Primera prueba interfaz grafica

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-#Primer commit
-#Segundo commit
-#Tercer commit
+
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+
+app = QApplication([])
+
+scene = QGraphicsScene()
+view = QGraphicsView(scene)
+view.setWindowTitle("Mapa del metro")
+view.resize(1096, 1280)
+
+mapa = QPixmap("MapaMetroCDMX.png")
+map_item = QGraphicsPixmapItem(mapa)
+scene.addItem(map_item)
+
+esfera = QGraphicsEllipseItem(500, 500, 30, 30)
+esfera.setBrush(QBrush(QColor(0, 0, 255, 180)))
+esfera.setZValue(100)
+scene.addItem(esfera)
+
+view.show()
+
+app.exec()
